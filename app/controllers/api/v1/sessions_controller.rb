dd87a5eba791a0f0ed9be731e3adfa0e02ca97af
Bruno Batista <brunobatista101@gmail.com>
2018-08-04 12:06:54 -0300

    adicionando sessoes

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::SessionsController < ApplicationController
+  
+  #Ao fazer login busca um usario pelo email, verifica o email e senha,caso de erro retorna 401
+  def create
+    user = User.find_by(email: sessions_params[:email])
+
+    if user && user.valid_password?(sessions_params[:password])
+      sign_in user, store: false
+      user.generate_authentication_token!
+      user.save
+      render json: user, status: 200
+    else
+      render json: { errors: 'Email ou senha inválidos' }, status: 401
+    end
+  end
+
+  #apaga uma sessao de usuario,ou seja fazer logout
+  def destroy
+    user = User.find_by(auth_token: params[:id])
+    user.generate_authentication_token!
+    user.save
+    head 204
+  end
+
+  private
+
+  #parametros permitidos no json de session
+  def sessions_params
+    params.require(:session).permit(:email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 require 'api_version_contraint'
 Rails.application.routes.draw do
-  #devise_for :users
+  #quero usa o recurso devise_for apenas para sessoes e o controle e o sessions
+  devise_for :users, only: [:sessions], controller: { sessions: 'api/v1/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json }, constraints: {subdomain: 'api'}, path: '/' do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resources :users, only: [:index, :show, :create, :update, :destroy]
+      resources :sessions, only: [:create, :destroy]
     end
   end
 end
