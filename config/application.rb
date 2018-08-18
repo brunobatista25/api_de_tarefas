b4b51e1985127445c272bca271f7147d8b0f463c
brunobatista25 <brunobatista101@gmail.com>
2018-08-18 12:12:20 -0300

    adicionando traducao pt-br

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,6 @@ module ApiTeste
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.i18n.default_locale = :'pt_BR'
+    config.i18n.default_locale = :'pt-BR'
   end
 end
